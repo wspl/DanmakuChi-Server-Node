581f90707c6364febd0678b4d1b2f919e2d2b7f3
Plutonist <widesense.plqws@gmail.com>
2015-10-24 10:37:50 +0800

    fix case

---
+++ src/Socket.js
@@ -0,0 +1,25 @@
+import SocketIO from 'socket.io';
+import Config from './Config';
+
+const io = SocketIO(Config.socket.listenPort);
+
+io.on('connection', function (socket) {
+  socket.on('channel', (channel) => {
+    //console.log(channel.toLowerCase());
+    process.on('message', (data) => {
+      if (data.type === 'CHANNEL_ISEXIST') {
+        if (data.isExist) {
+          socket.emit('channel', 'ok');
+        } else {
+          socket.emit('channel', 'no');
+        }
+      } else if (data.type === 'DANMAKU_SEND') {
+        socket.emit(channel, data.body);
+      }
+    });
+    process.send({
+      type: 'CHANNEL_ISEXIST',
+      channel: channel.toLowerCase()
+    });
+  });
+});
